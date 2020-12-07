40743a96f14676ee62c03d7683e84262a42800a4
Marcos Goyret <marcosgoyret@gmail.com>
2020-12-07 03:49:54 -0300

    lso archivos de canciones son pruebas, no tienen nada adentro

---
+++ GUILBass/inc/dialogjugar.h
@@ -0,0 +1,30 @@
+#ifndef DIALOGJUGAR_H
+#define DIALOGJUGAR_H
+
+#include <QDialog>
+#include <QDir>
+#include <QDebug>
+
+namespace Ui {
+class DialogJugar;
+}
+
+class DialogJugar : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogJugar(QWidget *parent = nullptr);
+    ~DialogJugar();
+
+    inline QString getNombreCancion( void ) { return nombreCancion; }
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::DialogJugar *ui;
+    QString nombreCancion;
+};
+
+#endif // DIALOGJUGAR_H
---
+++ GUILBass/src/dialogjugar.cpp
@@ -0,0 +1,31 @@
+#include "dialogjugar.h"
+#include "ui_dialogjugar.h"
+
+DialogJugar::DialogJugar(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DialogJugar)
+{
+    ui->setupUi(this);
+    ui->CBcanciones->clear();
+    nombreCancion = "";
+
+    QStringList lista = QDir("../media").entryList();
+    QString nombre;
+    for (int i = 0; i < lista.size(); i++)
+    {
+        nombre = lista.at(i);
+        if( (nombre != ".") && (nombre != "..") )
+            ui->CBcanciones->addItem(lista.at(i));
+    }
+}
+
+DialogJugar::~DialogJugar()
+{
+    delete ui;
+}
+
+void DialogJugar::on_pushButton_clicked()
+{
+    nombreCancion = ui->CBcanciones->currentText();
+    hide();
+}
